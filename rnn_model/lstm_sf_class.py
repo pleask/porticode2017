3bb5a59193902afe8f3e48d73ed8f0753f6d45d5
Bombedier <Bombedier@outlook.com>
2017-10-29 12:11:31 +0000

    small additions

--- rnn_model/lstm_sf_class.py
+++ rnn_model/lstm_sf_class.py
@@ -6,7 +6,7 @@ from keras.layers import Dense, LSTM
 import json
 
 
-class Lstm_sf(object):    
+class Loshc():    
     def __init__(self, tsteps=2, lahead=1, batch_size=1, epochs=10, d_ticks='data_list_saved.json', d_names='stock_names_saved.json'):
         self.tsteps = tsteps
         self.lahead = lahead
@@ -25,13 +25,13 @@ class Lstm_sf(object):
         self.data_ticks, self.data_names = self.data_loader()
         for ticker in symbol_list:
             self.state_test()
-            data_input = pd.DataFrame(self.choose_symb(ticker)) 
+            data_input = pd.DataFrame(self.choose_symb(ticker))
             self.data_plotter(data_input, ticker)
     
             ########
             print('Creating Stateful Model...')
             model_stateful = self.create_model(stateful=True)        
-            self.train_and_plot(data_input)
+            self.train_and_plot(data_input, model_stateful, ticker)
         
         pass
 
@@ -75,46 +75,46 @@ class Lstm_sf(object):
         return data_list
 
 
-        def data_plotter(self, data_input, ticker_symb):
-            ''' drop the nan '''
+    def data_plotter(self, data_input, ticker_symb):
+        ''' drop the nan '''
 
-            expected_output = data_input.rolling(window=self.tsteps, center=False).mean()
+        expected_output = data_input.rolling(window=self.tsteps, center=False).mean()
 
-            # when lahead > 1, need to convert the input to "rolling window view"
-            # https://docs.scipy.org/doc/numpy/reference/generated/numpy.repeat.html
-            if self.lahead > 1:
-                data_input = np.repeat(data_input.values, repeats=self.lahead, axis=1)
-                data_input = pd.DataFrame(data_input)
-                for i, c in enumerate(data_input.columns):
-                    data_input[c] = data_input[c].shift(i)
+        # when lahead > 1, need to convert the input to "rolling window view"
+        # https://docs.scipy.org/doc/numpy/reference/generated/numpy.repeat.html
+        if self.lahead > 1:
+            data_input = np.repeat(data_input.values, repeats=self.lahead, axis=1)
+            data_input = pd.DataFrame(data_input)
+            for i, c in enumerate(data_input.columns):
+                data_input[c] = data_input[c].shift(i)
 
-            # drop the nan
-            expected_output = expected_output[self.to_drop:]
-            data_input = data_input[self.to_drop:]
+        # drop the nan
+        expected_output = expected_output[self.to_drop:]
+        data_input = data_input[self.to_drop:]
 
-            print('Input shape:', data_input.shape)
-            print('Output shape:', expected_output.shape)
-            print('Input head: ')
-            print(data_input.head())
-            print('Output head: ')
-            print(expected_output.head())
-            print('Input tail: ')
-            print(data_input.tail())
-            print('Output tail: ')
-            print(expected_output.tail())
+        print('Input shape:', data_input.shape)
+        print('Output shape:', expected_output.shape)
+        print('Input head: ')
+        print(data_input.head())
+        print('Output head: ')
+        print(expected_output.head())
+        print('Input tail: ')
+        print(data_input.tail())
+        print('Output tail: ')
+        print(expected_output.tail())
 
-            print('Plotting input and expected output')
-            plt.plot(data_input[0][:10], '.')
-            plt.plot(expected_output[0][:10], '-')
-            plt.legend([ticker_symb, 'Expected output'])
-            plt.title(ticker_symb)
-            #plt.show()
-            plt.savefig(ticker_symb + '1.png')
-            plt.close()    
+        print('Plotting input and expected output')
+        plt.plot(data_input[0][:10], '.')
+        plt.plot(expected_output[0][:10], '-')
+        plt.legend([ticker_symb, 'Expected output'])
+        plt.title(ticker_symb)
+        #plt.show()
+        plt.savefig(ticker_symb + '1.png')
+        plt.close()    
 
+        pass
 
-
-            ######################
+        ######################
 
 
 
@@ -131,37 +131,44 @@ class Lstm_sf(object):
 
 
     # split train/test data
-def split_data(self, x, y, ratio: int = 0.8):
-    to_train = int(len(x) * ratio)
-    # tweak to match with batch_size
-    to_train -= to_train % self.batch_size
+    def split_data(self, x, y, ratio: int = 0.8):
+        to_train = int(len(x) * ratio)
+        print('to triain' , to_train)
+        # tweak to match with batch_size
+        to_train -= to_train % self.batch_size
 
-    x_train = x[:self.to_train]
-    y_train = y[:self.to_train]
-    x_test = x[self.to_train:]
-    y_test = y[self.to_train:]
+        x_train = x[:to_train]
+        y_train = y[:to_train]
+        x_test = x[to_train:]
+        y_test = y[to_train:]
 
-    # tweak to match with batch_size
-    to_drop = x.shape[0] % self.batch_size
-    if to_drop > 0:
-        x_test = x_test[:-1 * to_drop]
-        y_test = y_test[:-1 * to_drop]
+        # tweak to match with batch_size
+        to_drop = x.shape[0] % self.batch_size
+        if to_drop > 0:
+            x_test = x_test[:-1 * to_drop]
+            y_test = y_test[:-1 * to_drop]
 
-    # some reshaping
-    reshape_3 = lambda x: x.values.reshape((x.shape[0], x.shape[1], 1))
-    x_train = reshape_3(x_train)
-    x_test = reshape_3(x_test)
+        # some reshaping
+        reshape_3 = lambda x: x.values.reshape((x.shape[0], x.shape[1], 1))
+        x_train = reshape_3(x_train)
+        x_test = reshape_3(x_test)
 
-    reshape_2 = lambda x: x.values.reshape((x.shape[0], 1))
-    y_train = reshape_2(y_train)
-    y_test = reshape_2(y_test)
+        reshape_2 = lambda x: x.values.reshape((x.shape[0], 1))
+        y_train = reshape_2(y_train)
+        y_test = reshape_2(y_test)
 
-    return (x_train, y_train), (x_test, y_test)
+        return (x_train, y_train), (x_test, y_test)
 
 
-    def train_and_plot(self, data_input):
+    def train_and_plot(self, data_input, model_stateful, ticker):
         expected_output = data_input.rolling(window=self.tsteps, center=False).mean()
         (x_train, y_train), (x_test, y_test) = self.split_data(data_input, expected_output)
+        print('x_train.shape: ', x_train.shape)
+        print('y_train.shape: ', y_train.shape) 
+        print('x_test.shape: ', x_test.shape)
+        print('y_test.shape: ', y_test.shape)
+
+        print('Training')
         for i in range(self.epochs):
             #print('Epoch', i + 1, '/', epochs)
             # Note that the last state for sample i in a batch will
@@ -183,7 +190,7 @@ def split_data(self, x, y, ratio: int = 0.8):
         predicted_stateful = model_stateful.predict(x_test, batch_size=self.batch_size)
 
         print('Creating Stateless Model...')
-        model_stateless = create_model(stateful=False)
+        model_stateless = self.create_model(stateful=False)
 
         print('Training')
         model_stateless.fit(x_train,
@@ -206,7 +213,7 @@ def split_data(self, x, y, ratio: int = 0.8):
         plt.subplot(3, 1, 2)
         # drop the first "tsteps-1" because it is not possible to predict them
         # since the "previous" timesteps to use do not exist
-        plt.plot((y_test - predicted_stateful).flatten()[tsteps - 1:])
+        plt.plot((y_test - predicted_stateful).flatten()[self.tsteps - 1:])
         plt.title('Stateful: Expected - Predicted')
         plt.subplot(3, 1, 3)
         plt.plot((y_test - predicted_stateless).flatten())
@@ -214,3 +221,5 @@ def split_data(self, x, y, ratio: int = 0.8):
         #plt.show()
         plt.savefig(ticker + '2.png')
         plt.close()
+
+        pass
